efa85ccc8683e0e495937c834a15b0e0aa539f1a
Bhavyaraj Jaiswal <ideal.bhavyaraj1802@gmail.com>
2025-05-02 11:56:46 +0530

    Creating cart item edit form and completed profile form

--- src/utils/schemas/formValidations.ts
+++ src/utils/schemas/formValidations.ts
@@ -1,30 +1,77 @@
-import * as yup from "yup"
+import * as yup from "yup";
 
 export const validationLogin = yup.object().shape({
-  email: yup.string().email("Invalid email address").required("This field is required"),
+  email: yup
+    .string()
+    .email("Invalid email address")
+    .required("This field is required"),
   password: yup.string().min(6).required("This field is required"),
-})
+});
 
 export const validationRegister = yup.object().shape({
-  firstName: yup.string().min(2, "Name must have at least 2 characters").required("This field is required"),
-  lastName: yup.string().min(2, "Name must have at least 2 characters").required("This field is required"),
-  address: yup.string().min(2, "Address must have at least 2 characters").required("This field is required"),
-  email: yup.string().email("Invalid email address").required("This field is required"),
-  password: yup.string().min(6, "A password deve ter 6 caracteres no mínimo").required("This field is required"),
-  confirmPassword: yup.string().oneOf([yup.ref("password"), null], "Passwords do not match"),
-  phone: yup.string().matches(
-    /^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$/,
-    "Invalid Phone"
-  ).required("This field is required")
-})
+  firstName: yup
+    .string()
+    .min(2, "Name must have at least 2 characters")
+    .required("This field is required"),
+  lastName: yup
+    .string()
+    .min(2, "Name must have at least 2 characters")
+    .required("This field is required"),
+  address: yup
+    .string()
+    .min(2, "Address must have at least 2 characters")
+    .required("This field is required"),
+  email: yup
+    .string()
+    .email("Invalid email address")
+    .required("This field is required"),
+  password: yup
+    .string()
+    .min(6, "A password deve ter 6 caracteres no mínimo")
+    .required("This field is required"),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "Passwords do not match"),
+  phone: yup
+    .string()
+    .matches(
+      /^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$/,
+      "Invalid Phone"
+    )
+    .required("This field is required"),
+});
 export const validationProfile = yup.object().shape({
   image: yup.string().min(1).optional(),
-  fullName: yup.string().min(5, "Name must have at least 5 characters").required("This field is required"),
-  address: yup.string().min(2, "Address must have at least 2 characters").required("This field is required"),
-  email: yup.string().email("Invalid email address").required("This field is required"),
-  password: yup.string().min(6, "A password deve ter 6 caracteres no mínimo").required("This field is required"),
-  phone: yup.string().matches(
-    /^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$/,
-    "Invalid Phone"
-  ).required("This field is required")
-})
+  fullName: yup
+    .string()
+    .min(5, "Name must have at least 5 characters")
+    .required("This field is required"),
+  address: yup
+    .string()
+    .min(2, "Address must have at least 2 characters")
+    .required("This field is required"),
+  email: yup
+    .string()
+    .email("Invalid email address")
+    .required("This field is required"),
+  password: yup
+    .string()
+    .min(6, "A password deve ter 6 caracteres no mínimo")
+    .required("This field is required"),
+  phone: yup
+    .string()
+    .matches(
+      /^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$/,
+      "Invalid Phone"
+    )
+    .required("This field is required"),
+});
+
+export const validationEditCartItem = yup.object().shape({
+  size: yup.string(),
+  border: yup.bool(),
+  quantity: yup
+    .number()
+    .min(1, "Value must be greater than or equal to 1")
+    .required("This field is required"),
+});
